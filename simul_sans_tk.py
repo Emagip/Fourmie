2084b60184eaa06825babf6250d15ee926a0c881
Emagip <emagip@pm.me>
2024-05-02 13:28:05 +0200

    Documentation simul_sans_tk.py

--- simul_sans_tk.py
+++ simul_sans_tk.py
@@ -3,6 +3,9 @@ import Cases as cases
 
 
 def deplacement(coords):
+    """fonction metant a jour la position de la fourmie sur le plateau
+    prend en entrée les coordonnées actuelle de la fourmie
+    renvoie les nouvelles coordonnées"""
     direction = choice(plateau[coords[0]][coords[1]]['can_go'])
     coords[0] += direction[0]
     coords[1] += direction[1]
@@ -10,6 +13,11 @@ def deplacement(coords):
 
 
 def recupere_bonbon(fourmieAsBonbon, coords):
+    """Fcontion permettant a la fourmie de recupere un bonbon
+    prend en entrée:
+        la valeur actuelle de fourmieAsBonbon
+        les coordonnées actuelle de la fourmie
+    renvoie la nouvelle valeur de fourmieAsBonbon"""
     plateau[coords[1]][coords[0]]['HasCandy'] = False
     # ajout fourmie qui porte un bonbon
     fourmieAsBonbon = True
@@ -17,12 +25,20 @@ def recupere_bonbon(fourmieAsBonbon, coords):
 
 
 def pose_bonbon(CaseHautBonbon, coords, fourmieAsBonbon):
+    """Fonction permettant a la fourmie de poser un bonbon
+    prend en entrée :
+        la valeur actuelle de CaseHautBonbon
+        la valeur actuelle de fourmieAsBonbon
+        les coordonnées actuelle de la fourmie
+    renvoie la nouvelle valeur de CaseHautBonbon"""
     CaseHautBonbon[coords[0]] = True
     fourmieAsBonbon = False
     return fourmieAsBonbon
 
 
 def main():
+    """Boucle principale pour la simulation de la fourmie
+    ne prend pas d'arguments et renvoie le compteur de mouvements"""
     global plateau
     plateau = cases.set()
     compteur = 0
