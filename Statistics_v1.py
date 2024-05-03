116ecb8102ba4ffc0d1107df208d026ab5ee0d42
Emagip <emagip@pm.me>
2024-05-03 14:37:23 +0200

    Code de Fabian

--- Statistics_v1.py
+++ Statistics_v1.py
@@ -1,12 +1,11 @@
 from simul_sans_tk import *
+import simul_tk_v2 as simu
+
+
 import matplotlib.pyplot as plt
 from tkinter import *
 import numpy as np
 
-interface = Tk()
-
-interface.title("Bienvenue chez les ch'ti")
-interface.config(bg='blue')
 
 
 def graphique():
@@ -19,18 +18,30 @@ def graphique():
     plt.show()
 
 def moyenne():
-    moy = round(mouv/simu, 2)
+    moy = round(mouv/int(simu), 2)
     return moy
 
 def simulations():
-    global nb_simu, nb_mouv, mouv, simu, moy_len
-    simu = int(input("donner le nombre de simulation à faire"))
-    nb_simu = [y for y in range(simu)]
+    global nb_simu, nb_mouv, mouv, simu, moy_len,simu_var
+    '''
+    simu_var = StringVar()
+    simu_label = Label(simu.Cadre, text='donner le nombre de simulation à faire ') 
+    simu_label.pack()
+    simu_entry = Entry(Fond, textvariable= simu_var)
+    simu_entry.pack()
+    sub_btn=Button(Fond,text = 'Submit', command = submit)
+    sub_btn.pack()
+    '''
+    
+    simu = int(input('nombre de simulations à effectuer : '))
+
+    nb_simu = [y for y in range(int(simu))]
     mouv = 0 
     nb_mouv = []
     moy_len = []
-    for i in range(simu):
-        cpt = main()
+
+    for i in range(int(simu)):
+        cpt = main_sans_tk()
         print(cpt)
         mouv+=cpt
         nb_mouv.append(cpt)
@@ -39,16 +50,18 @@ def simulations():
     print("moyenne :", moyenne())
     graphique()
 
+
 def simuTK():
-    import simul_tk
+    global interface
+    simu.main_tk()
 
-Cadre = Canvas(interface,bg="black",width=1000, height = 1000)
+def submit():
+    nb_deboucle = simu_var.get()
+    return nb_deboucle
 
-JeuAffichage = Button(Cadre,text='Lancer une simulation avec interface graphique', command = simuTK)
+JeuAffichage = Button(simu.Fond,text='Lancer une simulation avec interface graphique', command = simuTK)
 JeuAffichage.pack()
-JeuGraphique = Button(Cadre,text='Lancer plusieurs simulations et obtenir des statistics', command = simulations)
+JeuGraphique = Button(simu.Fond,text='Lancer plusieurs simulations et obtenir des statistics', command = simulations)
 JeuGraphique.pack()
-
-Cadre.pack()
-
-interface.mainloop()
+simu.Fond.pack()
+simu.interface.mainloop()
